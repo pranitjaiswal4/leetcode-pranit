e352aba1beb27c813fc064f48bae3284323fdfe4
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2021-11-04 01:20:24 -0500

    Time: 32 ms (86.36%), Space: 14.5 MB (17.58%) - LeetHub

--- symmetric-tree/symmetric-tree.py
+++ symmetric-tree/symmetric-tree.py
@@ -7,39 +7,10 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
-        ans_left = []
-        ans_right = []
-        
-        def inorder(node, level):
+        def check(n1, n2):
+            if not n1 and not n2: return True
+            if not n1 or not n2: return False
             
-            if not node:
-                return
-            
-            level += 1
-            
-            inorder(node.left, level)
-            ans_left.append(str(node.val) + str(level))
-            inorder(node.right, level)
+            return n1.val == n2.val and check(n1.left, n2.right) and check(n1.right, n2.left)
         
-        def rev_inorder(node, level):
-            if not node:
-                return
-            
-            level += 1
-            
-            rev_inorder(node.right, level)
-            ans_right.append(str(node.val) + str(level))
-            rev_inorder(node.left, level)
-        
-        
-        inorder(root, 0)
-        rev_inorder(root, 0)
-        
-        # print(ans_left)
-        # print(ans_right)
-        
-        if ans_left == ans_right:
-            return True
-        
-        return False
-        
+        return check(root, root)
