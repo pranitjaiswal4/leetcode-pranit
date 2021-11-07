c5db66f51388913db6328cadb7644731d7d5f649
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2021-11-06 22:52:23 -0500

    Solution updated - Space complexity lowered

--- convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -4,29 +4,57 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         
-        def processTree(numlist):
-            root = None
+        def processTree(start, end):
+            if start > end:
+                return None
             
-            if len(numlist) > 1:
-                mid = len(numlist) // 2
-                root = TreeNode(numlist[mid])
-                leftList = numlist[:mid]
-                rightList = numlist[mid+1:]
-            
-                # print(leftList, rightList)
+            mid = (start + (end - start) // 2) 
+            root = TreeNode(nums[mid])
 
-                if leftList:
-                    root.left = processTree(leftList)
-
-                if rightList:
-                    root.right = processTree(rightList)
+            root.left = processTree(start, mid - 1)
+            root.right = processTree(mid + 1, end)
                     
-            else:
-                root = TreeNode(numlist[0])
-                
             return root
             
-        return processTree(nums)
+        return processTree(0, len(nums) - 1)
+
+
+# ALTERNATE SOLUTION
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+#         def processTree(numlist):
+#             root = None
+            
+#             if len(numlist) > 1:
+#                 mid = len(numlist) // 2
+#                 root = TreeNode(numlist[mid])
+#                 leftList = numlist[:mid]
+#                 rightList = numlist[mid+1:]
+            
+#                 # print(leftList, rightList)
+
+#                 if leftList:
+#                     root.left = processTree(leftList)
+
+#                 if rightList:
+#                     root.right = processTree(rightList)
+                    
+#             else:
+#                 root = TreeNode(numlist[0])
+                
+#             return root
+            
+#         return processTree(nums)
+
