1407a37da7958ea3aa3c41c4b4f1e1c2bc7fc82a
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2021-11-23 23:24:53 -0600

    Time: 123 ms (12.23%), Space: 27.8 MB (22.69%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -8,27 +8,31 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        common = None
-        
+        found = False
+        lca = None
+
         def dfs(node):
-            nonlocal common
-            
-            if not node: return 0
             count = 0
-            
-            if not common:
+            nonlocal found
+            nonlocal lca
+
+            if not node:
+                return 0
+
+            if node.val == p.val or node.val == q.val:
+                count += 1
+                
+            if not found and node.left:
                 count += dfs(node.left)
-            if not common:
+
+            if not found and node.right:
                 count += dfs(node.right)
 
-            if node == p: count += 1
-            if node == q: count += 1
-            
-            if not common and count >= 2:
-                common = node
-            
+            if not found and count == 2:
+                found = True
+                lca = node
+
             return count
-        
+
         dfs(root)
-        
-        return common
+        return lca
