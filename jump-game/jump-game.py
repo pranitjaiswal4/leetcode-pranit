0d23dcd8ebcff177c3cf990afb2c36d8f61cbdbe
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2021-11-29 02:05:40 -0600

    Time: 528 ms (44.99%), Space: 35.5 MB (5.02%) - LeetHub

---
+++ jump-game/jump-game.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+        
+        if nums[0] >= n - 1:
+            return True
+        
+        valid = False
+        for i in range(n-1):
+            if i + nums[i] >= n - 1:
+                valid = True
+        
+        def checkJump(i, jump_no):
+            if i < n:
+                if i == n - 1:
+                    t[i] = True
+                    return t[i]
+
+                if jump_no == nums[i]:
+                    t[i] = False
+                    return t[i]
+
+                if i + (nums[i] - jump_no) == n - 1:
+                    t[i] = True
+                    return t[i]
+
+                if t[i] != -1:
+                    return t[i]
+
+                t[i] = checkJump(i + (nums[i] - jump_no), 0) or checkJump(i, jump_no+1)
+                return t[i]
+        
+        t = [-1 for i in range(n+1)]
+        
+        if valid:
+            return checkJump(0, 0)
