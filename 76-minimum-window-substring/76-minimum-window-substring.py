08614765c35dc7775df68764bcdbb49d3242098f
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2022-01-13 01:01:40 -0600

    Time: 84 ms (97.67%), Space: 15 MB (9.98%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,49 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m = len(s)
+        n = len(t)
+        
+        # s: PPAAAATTTTTTTTBBBCDDEAFG -> A:1, B: -2, C: 0, D: 0
+        # t: ABCDD
+        
+        minWinLen = m + 1
+        output = ""
+
+        freq = dict()
+        
+        for c in t:
+            freq[c] = freq.get(c, 0) + 1
+        
+        count = len(freq)
+        i, j = 0, 0
+        
+        while j < m:
+            
+            if s[j] in freq:
+                freq[s[j]] -= 1
+                
+                if freq[s[j]] == 0:
+                    count -= 1
+            
+            while count == 0:
+                
+                if s[i] in freq:
+                    
+                    freq[s[i]] += 1
+                    
+                    if freq[s[i]] == 1:
+                        count += 1
+                    
+                winLen = j - i + 1
+                
+                if winLen < minWinLen:
+                    minWinLen = winLen
+                    output = s[i:j+1]
+                
+                i += 1
+            
+            j += 1
+            
+        
+        # print(freq)
+        return output
