463def7df661cd1bc495efd1ebeeded9a5f91571
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2022-01-24 00:54:29 -0600

    Time: 116 ms (40.16%), Space: 15.3 MB (95.88%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        from collections import deque
+        q = deque()
+        inorder = dict()
+        outorder = dict()
+        result = list()
+        
+        def bfs():
+            while q:
+                node = q.popleft()
+                result.append(node)
+                
+                for child in outorder[node]:
+                    inorder[child] -= 1
+                    
+                    if inorder[child] == 0:
+                        q.append(child)
+                    
+        for i in range(numCourses):
+            inorder[i] = 0
+            outorder[i] = []
+        
+        for course, prereq in prerequisites:
+            inorder[course] += 1              
+            outorder[prereq].append(course)
+        
+        for course in inorder:
+            if inorder[course] == 0:
+                q.append(course)
+
+        if not q:
+            return []
+        
+        bfs()
+        
+        if len(result) != numCourses:
+            return []
+        
+        return result
+        
