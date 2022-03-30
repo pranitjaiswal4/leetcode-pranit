587d0a95242be21917e6789f48e460c9932d3693
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2022-03-30 00:26:20 -0500

    Update 20-valid-parentheses.py

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -14,3 +14,26 @@ class Solution:
                     return False                        
         
         return not stack
+    
+    
+#     class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = list()
+#         braces = { ')': '(', '}' : '{', ']' : '['}
+        
+#         for i in range(len(s) - 1, -1, -1):
+            
+#             if s[i] in braces:
+#                 stack.append(s[i])
+#             else:
+#                 if stack:
+#                     top = stack[-1]
+                
+#                     if s[i] == braces[top]:
+#                         stack.pop()
+#                     else:
+#                         return False
+#                 else:
+#                     return False
+                
+#         return True if not stack else False
