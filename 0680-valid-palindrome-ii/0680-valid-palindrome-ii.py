a44d199021ced7f0b394aa4bd90d7227bfd1383d
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2024-04-18 16:44:18 -0700

    Time: 111 ms (33.27%), Space: 16.9 MB (92.95%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        deleted = 0
+        sub1 = sub2 = ""
+
+        def checkPalindrome(str):
+            nonlocal deleted
+            nonlocal sub1, sub2
+            i = 0 
+            j = len(str) - 1
+            while i <= j:
+                if str[i] != str[j]:
+                    if deleted == 0:
+                        sub1 = str[i+1:j+1]
+                        sub2 = str[i:j]
+                        print(sub1, sub2)
+                        deleted = 1
+                    return False
+                
+                i += 1
+                j -= 1
+            return True
+
+        if checkPalindrome(s):
+            return True
+
+        return checkPalindrome(sub1) or checkPalindrome(sub2)
