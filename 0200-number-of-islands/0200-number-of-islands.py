fa7a022391f949076387df33480f31db24d9a2c3
Pranit Jaiswal <60540780+pranitjaiswal4@users.noreply.github.com>
2024-04-29 21:49:18 -0700

    Time: 252 ms (42.72%), Space: 18.8 MB (81.55%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        m = len(grid)
+        n = len(grid[0])
+
+        directions = [ [-1, 0], [1, 0], [0, -1], [0, 1] ]
+        isBoundary = lambda i, j :  0 <= i < m and 0 <= j < n
+        isValid = lambda i, j : grid[i][j] == '1'
+
+        def dfs(i, j):
+            grid[i][j] = '#'
+
+            for x, y in directions:
+                newX = i + x
+                newY = j + y
+
+                if isBoundary(newX, newY):
+                    if isValid(newX, newY):
+                        dfs(newX, newY)
+        
+        for i in range(m):
+            for j in range(n):
+                if isValid(i, j):
+                    count += 1
+                    dfs(i, j)            
+
+        return count
